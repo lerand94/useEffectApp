ca50db6da0599ec9b3118ed9f37355c6661181b3
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-24 15:41:23 +0300

    added cleanup function for form validity.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const timer = setTimeout(() => {
+      console.log("checking validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("cleanup");
+      clearTimeout(timer);
+    };
   }, [enteredPassword, enteredEmail]);
 
   const emailChangeHandler = (event) => {
